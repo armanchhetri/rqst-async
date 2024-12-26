ada8e43773a231297e56ec71dd0c070857d25a70
Arman Chhetri <armanchhetri44@gmail.com>
2024-12-26 17:51:33 +0545

    feat: rutime from 4 to 2 seconds

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,6 +1,8 @@
 use miniserve::{http::StatusCode, Content, Request, Response};
 use serde::{Deserialize, Serialize};
 
+use chatbot::{gen_random_number, query_chat};
+
 async fn index(_req: Request) -> Response {
     let content = include_str!("../index.html").to_string();
     Ok(Content::Html(content))
@@ -18,9 +20,11 @@ async fn chat(req: Request) -> Response {
     let Ok(mut messages) = serde_json::from_str::<Messages>(&body) else {
         return Err(StatusCode::INTERNAL_SERVER_ERROR);
     };
-    messages
-        .messages
-        .push("And how does that make you feel?".into());
+    let index = gen_random_number();
+    let candidates = query_chat(&messages.messages);
+    let (index, candidates) = tokio::join!(index, candidates);
+
+    messages.messages.push(candidates[index % 2].clone());
     Ok(Content::Json(serde_json::to_string(&messages).unwrap()))
 }
 
